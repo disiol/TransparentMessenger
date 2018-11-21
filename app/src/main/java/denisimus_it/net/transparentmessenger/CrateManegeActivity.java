8fb5b5b46bcab43d398832cc7a357eaa8ed204f5
denis <deoniisii@gmail.com>
2018-11-21 14:21:31 +0200

    Initial commit

---
+++ app/src/main/java/denisimus_it/net/transparentmessenger/CrateManegeActivity.java
@@ -0,0 +1,13 @@
+package denisimus_it.net.transparentmessenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrateManegeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crate_manege);
+    }
+}
---
+++ app/src/main/java/denisimus_it/net/transparentmessenger/RecievedMessageActivity.java
@@ -0,0 +1,13 @@
+package denisimus_it.net.transparentmessenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecievedMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recieved_message);
+    }
+}
